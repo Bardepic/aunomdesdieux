129e62d23f5a20d7d86d285ebc3fa319cb38b0d2
Ouriel Lahoussaye <lahoussaye.ouriel@gmail.com>
2019-08-17 11:19:13 +0200

    mise à jour 4

--- wp-content/themes/andd/header.php
+++ wp-content/themes/andd/header.php
@@ -3,19 +3,19 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/dist/img/favicon.png" />
- <meta property="og:title" content="BeLib"/>
- <meta property="og:image" content="https://belib.org/wp-content/themes/belib/dist/img/banner.jpg"/>
- <meta property="og:description" content="Rencontrez des libertins où que vous soyez : au restaurant, au cinéma, à la plage ou dans les embouteillages, sans même toucher à votre smartphone."/>
+ <meta property="og:title" content="Au Nom Des Dieux"/>
+ <meta property="og:image" content="http://aunomdesdieux.fr/wp-content/uploads/2019/08/Campagne-Facebook-Ads.png"/>
+ <meta property="og:description" content="La saga fantastique par Gautier Durrieu de Madron - comment réagiriez vous au découvrant que toutes les légendes antiques étaient vraies ?"/>
  <meta property="og:type" content="website"/>
- <meta property="og:url" content="https://belib.org"/>
+ <meta property="og:url" content="https://aunomdesdieux.fr"/>
  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
  <link href="https://fonts.googleapis.com/css?family=Buenard:300,400,700,900&display=swap" rel="stylesheet">
 
  <meta name="twitter:card" content="summary" />
- <meta name="twitter:image" content="https://belib.org/wp-content/themes/belib/dist/img/banner.jpg" />
- <meta name="twitter:image:alt" content="Adoptez le Geo-Libertinage"/>
- <meta name="twitter:title" content="Belib"/>
- <meta name="twitter:description" content="Rencontrez des libertins où que vous soyez : au restaurant, au cinéma, à la plage ou dans les embouteillages, sans même toucher à votre smartphone." />
+ <meta name="twitter:image" content="http://aunomdesdieux.fr/wp-content/uploads/2019/08/Campagne-Facebook-Ads.png" />
+ <meta name="twitter:image:alt" content="Plongez dans la saga fantastique Au Nom des Dieux"/>
+ <meta name="twitter:title" content="Au Nom Des Dieux"/>
+ <meta name="twitter:description" content="La saga fantastique par Gautier Durrieu de Madron - comment réagiriez vous au découvrant que toutes les légendes antiques étaient vraies ?"" />
 
 </head>
 <?php wp_head(); ?>
