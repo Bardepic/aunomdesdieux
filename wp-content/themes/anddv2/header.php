0280ca01ef0324a8644867b71e38585905682bef
Ouriel Lahoussaye <lahoussaye.ouriel@gmail.com>
2020-07-06 18:25:22 +0200

    mise à jour OG Image

--- wp-content/themes/anddv2/header.php
+++ wp-content/themes/anddv2/header.php
@@ -3,19 +3,19 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/dist/img/favicon.png" />
- <meta property="og:title" content="Au Nom Des Dieux"/>
- <meta property="og:image" content="http://aunomdesdieux.fr/wp-content/uploads/2019/08/Campagne-Facebook-Ads.png"/>
- <meta property="og:description" content="La saga fantastique par Gautier Durrieu de Madron - comment réagiriez vous au découvrant que toutes les légendes antiques étaient vraies ?"/>
+ <meta property="og:title" content="Au Nom Des Dieux - L'univers uchronique qui dépoussière les mythologies"/>
+ <meta property="og:image" content="http://aunomdesdieux.fr/wp-content/uploads/2020/07/ogimage.jpg"/>
+ <meta property="og:description" content="Au crépuscule du monde, les anciennes mythologies ont encore leur mot à dire... Une Saga Fantastique à la française, par Gautier Durrieu de Madron, auteur et passionné de mythologie."/>
  <meta property="og:type" content="website"/>
  <meta property="og:url" content="https://aunomdesdieux.fr"/>
  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,900&display=swap" rel="stylesheet">
  <link href="https://fonts.googleapis.com/css?family=Buenard:300,400,700,900&display=swap" rel="stylesheet">
 
  <meta name="twitter:card" content="summary" />
- <meta name="twitter:image" content="http://aunomdesdieux.fr/wp-content/uploads/2019/08/Campagne-Facebook-Ads.png" />
+ <meta name="twitter:image" content="http://aunomdesdieux.fr/wp-content/uploads/2020/07/ogimage.jpg" />
  <meta name="twitter:image:alt" content="Plongez dans la saga fantastique Au Nom des Dieux"/>
- <meta name="twitter:title" content="Au Nom Des Dieux"/>
- <meta name="twitter:description" content="La saga fantastique par Gautier Durrieu de Madron - comment réagiriez vous au découvrant que toutes les légendes antiques étaient vraies ?"" />
+ <meta name="twitter:title" content="Au Nom Des Dieux - L'univers uchronique qui dépoussière les mythologies"/>
+ <meta name="twitter:description" content="Au crépuscule du monde, les anciennes mythologies ont encore leur mot à dire... Une Saga Fantastique à la française, par Gautier Durrieu de Madron, auteur et passionné de mythologie."" />
 
  
 <script src="<?php echo get_template_directory_uri();?>/dist/js/green-audio-player.js"></script>
